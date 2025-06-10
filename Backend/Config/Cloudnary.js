a5b36890c2f0f515af6c0982fbb203349fa66d7c
rishika846 <pupdejarishika@gmail.com>
2025-06-10 17:55:09 +0530

    Update Cloudnary.js

--- Backend/Config/Cloudnary.js
+++ Backend/Config/Cloudnary.js
@@ -2,8 +2,8 @@ import { v2 as cloudinary } from 'cloudinary'
 const connectCloudinary= async ()=>{
 cloudinary.config({ 
   cloud_name: 'dhl4d7liv',
-  api_key: API_KEY, 
-  api_secret: API_SECRET
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET
 });
 }
 export default connectCloudinary
